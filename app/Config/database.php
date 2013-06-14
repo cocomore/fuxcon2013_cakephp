977e8a419a4ccf016abbcd8bdee520eb3e243ed4
Olav Schettler <olav.schettler@cocomore.com>
2013-06-14 13:44:27 +0200

    Database config included in repo; this is still in .gitignore

---
+++ app/Config/database.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This is core configuration file.
+ */
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'fuxcon',
+		'password' => 'fuxcon',
+		'database' => 'fuxcon2013_cakephp',
+		'prefix' => '',
+		'encoding' => 'utf8',
+	);
+}
